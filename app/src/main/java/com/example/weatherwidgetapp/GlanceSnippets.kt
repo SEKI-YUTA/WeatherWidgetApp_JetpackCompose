da09549e3e1ee533d3431d08e0bcf58e3e073b6e
SEKI-YUA <zard.since1991@gmail.com>
2023-09-03 18:23:56 +0900

    レイアウトをホーム画面に表示するまでは簡単にできた

--- app/src/main/java/com/example/weatherwidgetapp/GlanceSnippets.kt
+++ app/src/main/java/com/example/weatherwidgetapp/GlanceSnippets.kt
@@ -1,11 +1,24 @@
 package com.example.weatherwidgetapp
 
 import android.content.Context
-import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.glance.Button
 import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.action.actionStartActivity
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.provideContent
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.Row
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import androidx.glance.text.Text
+
+// 公式ページ
+//https://developer.android.com/jetpack/compose/glance/create-app-widget
 
 class MyAppWidgetReceiver: GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget
@@ -15,7 +28,31 @@ class MyAppWidgetReceiver: GlanceAppWidgetReceiver() {
 class MyAppWidget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
-            Text("Hello World!")
+            MyContent()
+        }
+    }
+
+    @Composable
+    private fun MyContent() {
+        Column(
+            modifier = GlanceModifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Where to?", modifier = GlanceModifier.padding(16.dp))
+            Row(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    text = "Home",
+                    onClick = actionStartActivity<MainActivity>(
+                    )
+               )
+                Button(
+                    text = "Work",
+                    onClick = actionStartActivity<MainActivity>()
+                )
+            }
         }
     }
 }
